768ffea29934b22a439273a0806221ca37670e3f
emmanesp <emmanesp13@gmail.com>
2022-06-26 15:59:54 -0600

    added files to git ignore, db connection added and get products endpoint created

---
+++ app/api/v1/endpoints/healthcheck_endpoints.py
@@ -0,0 +1,15 @@
+"""
+Healthcheck endpoint
+"""
+from fastapi import APIRouter, status
+
+router = APIRouter()
+
+
+@router.get("", status_code=status.HTTP_200_OK)
+def get_health_status():
+    """
+    Get the healthcheck of the app
+    """
+
+    return "working with version 0.1.0"
